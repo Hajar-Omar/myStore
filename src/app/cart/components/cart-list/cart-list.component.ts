1499e3e207fc75f79451b04264c365d9998a65e8
Hajar <hajaro@banquemisr.com>
2022-08-20 22:00:22 +0200

    update remove/add functionality

--- src/app/cart/components/cart-list/cart-list.component.ts
+++ src/app/cart/components/cart-list/cart-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from 'src/app/core/models/interfaces/product';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart-list',
@@ -10,7 +11,16 @@ export class CartListComponent {
   @Input() cart: IProduct[] = [];
   @Output() cartChange = new EventEmitter<IProduct[]>();
 
-  changeAmount() {
+  constructor(private cartService: CartService){
+  }
+
+  emitCart() {
     this.cartChange.emit(this.cart);
+  }
+
+  removeItem(item: IProduct){
+    this.cartService.removeFromCart(item);
+  this.cart = this.cartService.getCartDetails();
+this.emitCart();
   }
 }
--- src/app/cart/services/cart.service.ts
+++ src/app/cart/services/cart.service.ts
@@ -13,11 +13,20 @@ export class CartService {
   constructor() {}
 
   addToCart(product: IProduct) {
-    this.cart.push(product);
+    const isAddedBefore = this.cart.findIndex((e) => e.id === product.id);
+    if (isAddedBefore > -1) {
+      this.cart[isAddedBefore].selectedAmount = product.selectedAmount;
+    } else {
+      this.cart.push(product);
+    }
   }
 
   getCartDetails(): IProduct[] {
     return this.cart;
+  }
+
+  removeFromCart(item: IProduct) {
+    this.cart = this.cart.filter((e) => e.id !== item.id);
   }
 
   emitUser(val: IUser) {
