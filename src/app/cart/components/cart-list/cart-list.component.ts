453f013fdb211d4d189a72a4f945d13f0877a426
Hajar <hajaro@banquemisr.com>
2022-08-21 18:15:22 +0200

    add feedback when remove from cart

--- src/app/cart/components/cart-list/cart-list.component.ts
+++ src/app/cart/components/cart-list/cart-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IProduct } from 'src/app/core/models/interfaces/product';
 import { CartService } from '../../services/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cart-list',
@@ -11,16 +12,19 @@ export class CartListComponent {
   @Input() cart: IProduct[] = [];
   @Output() cartChange = new EventEmitter<IProduct[]>();
 
-  constructor(private cartService: CartService){
-  }
+  constructor(private cartService: CartService, private _snackBar: MatSnackBar) {}
 
   emitCart() {
     this.cartChange.emit(this.cart);
   }
 
-  removeItem(item: IProduct){
+  removeItem(item: IProduct) {
     this.cartService.removeFromCart(item);
-  this.cart = this.cartService.getCartDetails();
-this.emitCart();
+    this.cart = this.cartService.getCartDetails();
+    this.emitCart();
+    this._snackBar.open('Removed from Cart!', '', {
+      duration: 500,
+      verticalPosition: 'top',
+    });
   }
 }
